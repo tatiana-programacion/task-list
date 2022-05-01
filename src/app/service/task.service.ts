3594f5b11938efb7687442c2a2fd9f6fe5e0bbec
tatiana-programacion <tatiana.programacion@gmail.com>
2022-05-01 18:45:40 -0300

    Cambiar estado reminder

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+
+    "Content-Type": "application/json"
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +23,16 @@ export class TaskService {
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  deleteTask(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
   }
 
 }
